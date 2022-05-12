b5c4b6700bededf39ebd1902e84f41fe5779aa83
Daniel Ramirez <contact@dann.dev>
2022-05-12 17:16:35 -0500

    add Test: verify that method getStudentsHigherCredits return a valid list of students that have an higher score than requested

--- test/lib/controllers/StudentsController.test.js
+++ test/lib/controllers/StudentsController.test.js
@@ -30,4 +30,34 @@ describe("Test for Stutends Controllers", () => {
         expect(result[0]).toBe("Todd@visualpartnership.xyz");
         expect(result[result.length - 1]).toBe("Hays@visualpartnership.xyz");
     });
+
+    test(" Test to get Students with scores higher than sent (500)", () => {
+        const result = StudentController.getStudentsHigherCredits(500);
+        expect(result[0]).toStrictEqual({
+            "credits": 508,
+            "email": "Todd@visualpartnership.xyz",
+            "enrollments": [
+                "Visual Thinking Intermedio",
+                "Visual Thinking Avanzado",
+            ],
+            "haveCertification": true,
+            "id": "6264d5d89f1df827eb84bb23",
+            "name": "Warren",
+            "previousCourses": 1,
+        });
+
+        expect(result[result.length - 1]).toStrictEqual({
+            "credits": 552,
+            "email": "Shields@visualpartnership.xyz",
+            "enrollments": [
+                "Visual Thinking Avanzado",
+                "Visual Thinking Intermedio",
+            ],
+            "haveCertification": true,
+            "id": "6264d5d8e384c8da8ea5c16b",
+            "name": "Ware",
+            "previousCourses": 4,
+        });
+    });
+
 });
