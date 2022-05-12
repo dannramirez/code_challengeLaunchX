258b75552937553fba3e37228312162bbc882771
Daniel Ramirez <contact@dann.dev>
2022-05-12 15:08:24 -0500

    add Test: Test getStudentsHigherCredits Method from StudentService

--- test/lib/services/StudentService.test.js
+++ test/lib/services/StudentService.test.js
@@ -1,5 +1,5 @@
 const Reader = require("./../../../lib/util/Reader");
-const StudentService = require("../../../lib/services/studentService");
+const StudentService = require("./../../../lib/services/StudentService");
 const students = Reader.readJsonFile("./test/lib/services/student_test.json");
 
 describe("Test for ExplorerService", () => {
@@ -68,4 +68,40 @@ describe("Test for ExplorerService", () => {
         const emailStudentsCertificated = StudentService.getStudentsEmailsWithCertification(students);
         expect(emailStudentsCertificated).toStrictEqual(["Todd@visualpartnership.xyz", "Sexton@visualpartnership.xyz", "Sharlene@visualpartnership.xyz", "Howell@visualpartnership.xyz"]);
     });
+
+    test("Get Students with Higher credits than 500", () => {
+        const studentsHigherCredits = StudentService.getStudentsHigherCredits(students, 500);
+        expect(studentsHigherCredits).toStrictEqual([{
+            id: "6264d5d89f1df827eb84bb23",
+            name: "Warren",
+            email: "Todd@visualpartnership.xyz",
+            credits: 508,
+            enrollments: [
+                "Visual Thinking Intermedio",
+                "Visual Thinking Avanzado"
+            ],
+            previousCourses: 1,
+            haveCertification: true
+        },
+        {
+            id: "6264d5d85cf81c496446b67f",
+            name: "Lucinda",
+            email: "Sexton@visualpartnership.xyz",
+            credits: 677,
+            enrollments: ["Visual Thinking Avanzado"],
+            previousCourses: 6,
+            haveCertification: true
+        },
+        {
+            id: "6264d5d8dd1a0be4e249c662",
+            name: "Phillips",
+            email: "Camacho@visualpartnership.xyz",
+            credits: 973,
+            enrollments: ["Visual Thinking Intermedio"],
+            previousCourses: 8,
+            haveCertification: false
+        }
+        ]);
+    });
+
 });
