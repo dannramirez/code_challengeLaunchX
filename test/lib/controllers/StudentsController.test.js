d4d3b7a9ed4321cdf67ce93ef87ed2de3ee7b2bb
Daniel Ramirez <contact@dann.dev>
2022-05-12 17:13:22 -0500

    add Test: verify that method of StudentController return all students

---
+++ test/lib/controllers/StudentsController.test.js
@@ -0,0 +1,28 @@
+const StudentController = require("../../../lib/controllers/StudentsController");
+
+describe("Test for Stutends Controllers", () => {
+
+    test("Test to get Students", () => {
+        const result = StudentController.getStudents();
+        expect(result[0]).toStrictEqual({
+            "credits": 508,
+            "email": "Todd@visualpartnership.xyz",
+            "enrollments": ["Visual Thinking Intermedio", "Visual Thinking Avanzado"],
+            "haveCertification": true,
+            "id": "6264d5d89f1df827eb84bb23",
+            "name": "Warren",
+            "previousCourses": 1
+        });
+
+        expect(result[result.length - 1]).toStrictEqual({
+            "credits": 227,
+            "email": "Hays@visualpartnership.xyz",
+            "enrollments": ["Visual Thinking Intermedio", "Visual Thinking Básico"],
+            "haveCertification": true,
+            "id": "6264d5d82eb0f4917bd0d332",
+            "name": "Clare",
+            "previousCourses": 2
+        });
+    });
+
+});
