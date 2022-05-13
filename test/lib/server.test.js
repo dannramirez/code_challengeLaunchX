5ac608539d380d8ed05b7258b37d73943fec0148
Daniel Ramirez <contact@dann.dev>
2022-05-12 19:05:24 -0500

    add Test: verify if /students/certificated endpoint return a list of emails from students that have been certified

--- test/lib/server.test.js
+++ test/lib/server.test.js
@@ -43,4 +43,40 @@ describe("Test endpoint responses", () => {
         });
     });
 
+    test("Gets a list of emails from certified students /v1/students/certificated endpoint", async () => {
+        const response = await request.get("/v1/students/certificated");
+        expect(response.status).toBe(200);
+        expect(response.body).toStrictEqual([
+            "Todd@visualpartnership.xyz",
+            "Sexton@visualpartnership.xyz",
+            "Sharlene@visualpartnership.xyz",
+            "Howell@visualpartnership.xyz",
+            "Haynes@visualpartnership.xyz",
+            "Montoya@visualpartnership.xyz",
+            "Benton@visualpartnership.xyz",
+            "Dionne@visualpartnership.xyz",
+            "Mcpherson@visualpartnership.xyz",
+            "Gracie@visualpartnership.xyz",
+            "Ila@visualpartnership.xyz",
+            "Dorthy@visualpartnership.xyz",
+            "Mcfarland@visualpartnership.xyz",
+            "Maryann@visualpartnership.xyz",
+            "Alana@visualpartnership.xyz",
+            "Rosemary@visualpartnership.xyz",
+            "Keith@visualpartnership.xyz",
+            "Delaney@visualpartnership.xyz",
+            "Ball@visualpartnership.xyz",
+            "Sally@visualpartnership.xyz",
+            "Antoinette@visualpartnership.xyz",
+            "Juliette@visualpartnership.xyz",
+            "Harding@visualpartnership.xyz",
+            "Dixon@visualpartnership.xyz",
+            "Beulah@visualpartnership.xyz",
+            "Moses@visualpartnership.xyz",
+            "Shields@visualpartnership.xyz",
+            "Jewell@visualpartnership.xyz",
+            "Hays@visualpartnership.xyz",
+        ]);
+    });
+
 });
