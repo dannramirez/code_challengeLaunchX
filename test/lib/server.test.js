1caf9068517a7507cbe54e845a6f78d61873a129
Daniel Ramirez <contact@dann.dev>
2022-05-12 19:03:44 -0500

    add Test: verify if /students endpoint return all students

--- test/lib/server.test.js
+++ test/lib/server.test.js
@@ -19,4 +19,28 @@ describe("Test endpoint responses", () => {
         });
     });
 
+    test("Gets all students with /v1/students endpoint", async () => {
+        const response = await request.get("/v1/students");
+        expect(response.status).toBe(200);
+        expect(response.body[0]).toStrictEqual({
+            "credits": 508,
+            "email": "Todd@visualpartnership.xyz",
+            "enrollments": ["Visual Thinking Intermedio", "Visual Thinking Avanzado"],
+            "haveCertification": true,
+            "id": "6264d5d89f1df827eb84bb23",
+            "name": "Warren",
+            "previousCourses": 1
+        });
+
+        expect(response.body[response.body.length - 1]).toStrictEqual({
+            "credits": 227,
+            "email": "Hays@visualpartnership.xyz",
+            "enrollments": ["Visual Thinking Intermedio", "Visual Thinking Básico"],
+            "haveCertification": true,
+            "id": "6264d5d82eb0f4917bd0d332",
+            "name": "Clare",
+            "previousCourses": 2
+        });
+    });
+
 });
